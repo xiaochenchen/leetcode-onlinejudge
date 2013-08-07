213247748585dd9c39dbe022675747ccdf653772
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-07 15:20:32 -0400

    Create AddTwoNumbers.java

---
+++ src/main/java/AddTwoNumbers.java
@@ -0,0 +1,94 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class AddTwoNumbers 
+{
+  public LinkedList<Integer> addTwoNumbers(LinkedList<Integer> a, LinkedList<Integer> b)
+	{
+		LinkedList<Integer> result = new LinkedList<Integer>();
+		
+		ListIterator<Integer> itrA = a.listIterator();
+		ListIterator<Integer> itrB = b.listIterator();
+		
+		int carryOver = 0;
+		while(itrA.hasNext() && itrB.hasNext())
+		{
+			int sum = itrA.next() + itrB.next() + carryOver;
+			result.add(sum % 10);
+			carryOver = sum / 10;
+		}
+		while(itrA.hasNext())
+		{
+			int sum = itrA.next() + carryOver;
+			result.add(sum % 10);
+			carryOver = sum / 10;
+		}
+		while(itrB.hasNext())
+		{
+			int sum = itrB.next() + carryOver;
+			result.add(sum % 10);
+			carryOver = sum / 10;
+		}
+		
+		if(carryOver != 0)
+		{
+			result.add(carryOver);
+		}
+		
+		return result;
+	}
+
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+        val = x;
+        next = null;
+		}
+	}
+	
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+	{
+		ListNode result = null;
+		
+		int carryOver = 0;
+		ListNode curNode = null;
+		while(l1 != null || l2 != null)
+		{
+			int sum = 0;
+			if(l1 != null)
+			{
+				sum += l1.val;
+				l1 = l1.next;
+			}
+			
+			if(l2 != null)
+			{
+				sum += l2.val;
+				l2 = l2.next;
+			}
+			
+			sum += carryOver;
+
+			if(result == null)
+			{
+				result = new ListNode(sum % 10);
+				curNode = result;
+			}
+			else
+			{
+				curNode.next = new ListNode(sum % 10);
+				curNode = curNode.next;
+			}
+			
+			carryOver = sum / 10;
+		}
+		
+		if(carryOver != 0)
+		{
+			curNode.next = new ListNode(carryOver);
+		}
+		
+		return result;
+	}
+}
