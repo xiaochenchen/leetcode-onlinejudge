6a8aa53c3195d64850dd9cea3213ef152fce143a
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-08 21:05:09 -0400

    add LongestPalindromicSubstringTest

---
+++ src/test/java/LongestPalindromicSubstringTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xiaochen Chen
+ * Date: 8/7/13
+ * Time: 11:12 PM
+ */
+public class LongestPalindromicSubstringTest
+{
+    @Test
+    public void longestPalindromeDPTest()
+    {
+        LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
+        lps.longestPalindromeDP("");
+    }
+}
