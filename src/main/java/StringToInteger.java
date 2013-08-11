606943c8eb63c499bdf7d41ab2f6c4c88433f2c1
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-11 13:23:33 -0400

    add problem description for String to Integer

--- src/main/java/StringToInteger.java
+++ src/main/java/StringToInteger.java
@@ -3,6 +3,15 @@
  * User: Xiaochen Chen
  * Date: 8/11/13
  * Time: 11:38 AM
+ *
+ * Problem Description:
+ *
+ * Implement atoi to convert a string to an integer.
+ *
+ * Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+ *
+ * Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+ *
  */
 public class StringToInteger
 {
