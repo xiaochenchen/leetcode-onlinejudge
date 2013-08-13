8f1b838053d787971b7f37027f6ef8df7249aafe
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-13 01:29:55 -0400

    fix RegularExpressionMatching DP bug

--- src/main/java/RegularExpressionMatching.java
+++ src/main/java/RegularExpressionMatching.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 /**
  * Problem Description:
- * 
+ *
  * Implement regular expression matching with support for '.' and '*'.
  *
  * '.' Matches any single character.
@@ -19,129 +21,122 @@
  * isMatch("aa", ".*") ? true
  * isMatch("ab", ".*") ? true
  * isMatch("aab", "c*a*b") ? true
- * 
+ *
  * @author
  *
  */
-public class RegularExpressionMatching 
+public class RegularExpressionMatching
 {
-	public boolean isMatch(String s, String p)
-	{
-		// if either source or pattern is null, return false
-		if(s == null || p == null)
-		{
-			return false;
-		}	
-		
-		// if pattern is empty, return true only if source is also empty
-		if(p.isEmpty())
-		{
-			return s.isEmpty();
-		}
-		
-		// if single non * character: must match current character
-		// if next character is not '*': must match current character
-		if(p.length() == 1 || p.charAt(1) != '*')
-		{
-			if(p.charAt(0) == '*' || s.isEmpty())
-			{
-				return false;
-			}
-			return ( p.charAt(0) == s.charAt(0) || p.charAt(0) == '.' )
-					&& isMatch(s.substring(1), p.substring(1)) ;
-		}
-	
-		// next character is *
-		while(s.isEmpty() || p.charAt(0) == s.charAt(0) || p.charAt(0) == '.' )
-		{
-			if(isMatch(s, p.substring(2)))
-			{
-				return true;
-			}
-			else if(s.isEmpty())
-			{
-				return false;
-			}
-			
-			s = s.substring(1);
-		}
+    public boolean isMatch(String s, String p)
+    {
+        // if either source or pattern is null, return false
+        if(s == null || p == null)
+        {
+            return false;
+        }
 
-		return isMatch(s, p.substring(2));
-	}
-	
-		public boolean isMatchDP(String s, String p)
-	{
-		if(s == null || p == null)
-		{
-			return false;
-		}
-		
-		if(s.isEmpty() || p.isEmpty())
-		{
-			return s.equals(p);
-		}
-		
-		if(p.charAt(0) == '*')
-		{
-			return false;
-		}
-				
-		int n = p.length();
-		int m = s.length();
-		
-		boolean[][] a = new boolean[n][m];
-		
-		// initialize first row
-		// row number increases as source string grows
-		if(s.charAt(0) == p.charAt(0))
-		{
-			a[0][0] = true;
-		}
-		
-		// initialize first column
-		//for(int i = 1; i < n; ++i)
-		//{
-		//	if()
-		//}
-		
-		if(p.length() > 1 && p.charAt(1) == '*')
-		{
-			a[1][0] = a[0][0];
-		}
-		
-		for(int i = 1; i < n; ++i)
-		{
-			for(int j = i; j < m; ++j)
-			{
-				if(p.charAt(i) == '*')
-				{
-					a[i][j] = a[i-1][j]
-							|| ( a[i-1][j-1] && ( s.charAt(j-1) == s.charAt(j) ||  p.charAt(i-1) == '.' ) )
-									|| ( a[i][j-1] && s.charAt(j-1) == s.charAt(j) ) ;
-				}
-				else if(p.charAt(i) == '.')
-				{
-					a[i][j] = a[i-1][j-1];
-				}
-				else
-				{
-					a[i][j] = a[i-1][j-1] && ( p.charAt(i) == s.charAt(j) );
-				}
-			}
-		}
-		
-		System.out.println(Arrays.deepToString(a));
-		System.out.println(a[n-1][m-1]);
-		
-		return a[n-1][m-1];
-	}
-	
-	public boolean isMatchDPOL(String s, String p)
-	{
-        boolean[] lastRow = new boolean[s.length()+1]; 
-        boolean[] currRow = new boolean[s.length()+1]; 
+        // if pattern is empty, return true only if source is also empty
+        if(p.isEmpty())
+        {
+            return s.isEmpty();
+        }
+
+        // if single non * character: must match current character
+        // if next character is not '*': must match current character
+        if(p.length() == 1 || p.charAt(1) != '*')
+        {
+            if(p.charAt(0) == '*' || s.isEmpty())
+            {
+                return false;
+            }
+            return ( p.charAt(0) == s.charAt(0) || p.charAt(0) == '.' )
+                    && isMatch(s.substring(1), p.substring(1)) ;
+        }
+
+        // next character is *
+        while(s.isEmpty() || p.charAt(0) == s.charAt(0) || p.charAt(0) == '.' )
+        {
+            if(isMatch(s, p.substring(2)))
+            {
+                return true;
+            }
+            else if(s.isEmpty())
+            {
+                return false;
+            }
+
+            s = s.substring(1);
+        }
+
+        return isMatch(s, p.substring(2));
+    }
+
+    public boolean isMatchDP(String s, String p)
+    {
+        if(s == null || p == null)
+        {
+            return false;
+        }
+
+        if(p.isEmpty())
+        {
+            return s.isEmpty();
+        }
+
+        if(p.charAt(0) == '*')
+        {
+            return false;
+        }
+
+        int n = p.length();
+        int m = s.length();
+
+        boolean[][] a = new boolean[n+1][m+1];
+
+        // initialize first row
+        // empty p only matches empty s
+        a[0][0] = true;
+
+        // initialize first column
+        // for empty s, '*' and it's preceding character can be ignored for matching
+        for(int i = 1; i < n+1; ++i)
+        {
+        	if(p.charAt(i-1) == '*')
+            {
+                a[i][0] = a[i-2][0];
+            }
+        }
+
+        for(int i = 1; i < n+1; ++i)
+        {
+            for(int j = 1; j < m+1; ++j)
+            {
+                if(p.charAt(i-1) == '*')
+                {
+                    a[i][j] = a[i-2][j]
+                            || ( a[i-1][j-1] && ( p.charAt(i-2) == s.charAt(j-1) ||  p.charAt(i-2) == '.' ) )
+                            || ( a[i][j-1] && ( p.charAt(i-2) == s.charAt(j-1) || p.charAt(i-2) == '.' ) );
+                }
+                else if(p.charAt(i-1) == '.')
+                {
+                    a[i][j] = a[i-1][j-1];
+                }
+                else
+                {
+                    a[i][j] = a[i-1][j-1] && ( p.charAt(i-1) == s.charAt(j-1) );
+                }
+            }
+        }
+
+        return a[n][m];
+    }
+
+    public boolean isMatchDPOL(String s, String p)
+    {
+        boolean[] lastRow = new boolean[s.length()+1];
+        boolean[] currRow = new boolean[s.length()+1];
         boolean[] tmpRow;
- 
+
         // init star[] and pattern
         int cnt = 0;
         for (int i=0; i<p.length(); i++) {
@@ -160,7 +155,7 @@ public class RegularExpressionMatching
             }
         }
         p = sb.toString();
- 
+
         // DP
         lastRow[0] = true;
         for (int i=0; i<p.length(); i++) {
@@ -170,23 +165,23 @@ public class RegularExpressionMatching
             } else {
                 currRow[0] = false;
             }
- 
+
             for (int j=0; j<s.length(); j++) {
                 int rowIndex = j + 1;
- 
-                if (!star[i]) {        
+
+                if (!star[i]) {
                     if (p.charAt(i)=='.') {
                         if (lastRow[rowIndex-1]) {
                             currRow[rowIndex] = true;
                         } else {
-                            currRow[rowIndex] = false;                        
-                        }                    
+                            currRow[rowIndex] = false;
+                        }
                     } else {
                         if (p.charAt(i)==s.charAt(j) && lastRow[rowIndex-1]) {
                             currRow[rowIndex] = true;
                         } else {
-                            currRow[rowIndex] = false;                        
-                        }                        
+                            currRow[rowIndex] = false;
+                        }
                     }
                 } else {
                     if (lastRow[rowIndex]) {
@@ -201,12 +196,12 @@ public class RegularExpressionMatching
                         currRow[rowIndex] = false;
                     }
                 }
-            }            
+            }
             // swap lastRow and currRow
             tmpRow = lastRow;
             lastRow = currRow;
             currRow = tmpRow;
         }
-        return lastRow[lastRow.length-1];		
-	}
+        return lastRow[lastRow.length-1];
+    }
 }
---
+++ src/test/java/RegularExpressionMatchingTest.java
@@ -0,0 +1,35 @@
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xiaochen Chen
+ * Date: 8/13/13
+ * Time: 12:00 AM
+ */
+public class RegularExpressionMatchingTest {
+
+    RegularExpressionMatching rem = new RegularExpressionMatching();
+
+    @Before
+    public void setUp() throws Exception {
+        rem = new RegularExpressionMatching();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testIsMatchDP() throws Exception {
+        rem.isMatchDP("", ".*");
+    }
+
+    @Test
+    public void testIsMatchDPOL() throws Exception {
+        Assert.assertTrue(rem.isMatchDPOL("aaa", "ab*ac*a"));
+    }
+}
