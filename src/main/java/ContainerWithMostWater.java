d055376e7661595069d1e9b33a9a80f22a2d6acd
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-13 08:40:14 -0400

    write ContainerWithMostWater in both brute force and O(n) solution

---
+++ src/main/java/ContainerWithMostWater.java
@@ -0,0 +1,51 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xiaochen Chen
+ * Date: 8/13/13
+ * Time: 8:00 AM
+ */
+public class ContainerWithMostWater
+{
+    public int maxAreaBF(int[] height)
+    {
+        int maxArea = 0;
+        for(int i = 1; i < height.length; ++i)
+        {
+            for(int j = i; j < height.length; ++j)
+            {
+                int curArea = Math.min(height[j-i],height[j]) * i;
+                if(curArea > maxArea)
+                {
+                    maxArea = curArea;
+                }
+            }
+
+        }
+        return maxArea;
+    }
+
+    public int maxArea(int[] height)
+    {
+        int l = 0, r = height.length - 1;
+
+        int maxArea = 0;
+        while(l < r)
+        {
+            int curArea = Math.min(height[l],height[r]) * (r-l);
+            if(curArea > maxArea)
+            {
+                maxArea = curArea;
+            }
+            if(height[l] <= height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ src/test/java/ContainerWithMostWaterTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xiaochen Chen
+ * Date: 8/13/13
+ * Time: 8:35 AM
+ */
+public class ContainerWithMostWaterTest {
+    @Test
+    public void testMaxArea() throws Exception {
+        ContainerWithMostWater cww = new ContainerWithMostWater();
+        cww.maxArea(new int[]{1,3,1,2,3});
+    }
+}
