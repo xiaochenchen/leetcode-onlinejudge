f0cbc45b60b4c45d1b69f8a2c11477dccabaca6c
Xiaochen Chen <xiaochenchen.git@gmail.com>
2014-02-09 12:12:53 -0500

    code for remove duplicates from sorted array

---
+++ src/main/java/xcc/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,34 @@
+package xcc.leetcode;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+	public int removeDuplicates(int[] A)
+	{		
+		int curIndex = -1;
+		
+		for(int i = 0; i < A.length; i++)
+		{
+			if(i == 0)
+			{
+				curIndex = 0;
+				continue;
+			}
+			
+			while(i < A.length && A[i] == A[i-1]) 
+			{
+				i++;
+			}
+			
+			if(i == A.length)
+			{
+				break;
+			}
+			
+			// found the first non duplicates
+			A[++curIndex] = A[i];
+		}
+		
+		return curIndex + 1;
+	}
+}
---
+++ src/test/java/xcc/leetcode/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,14 @@
+package xcc.leetcode;
+
+import org.junit.Test;
+
+public class RemoveDuplicatesFromSortedArrayTest {
+
+	@Test
+	public void testRemoveDuplicates() {
+		RemoveDuplicatesFromSortedArray obj = new RemoveDuplicatesFromSortedArray();
+		System.out.println(obj.removeDuplicates(new int[] {1,1,2}));
+		
+	}
+
+}
