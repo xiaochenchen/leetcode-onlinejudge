0c930f4362747153129847870896e3f7dc555af3
Xiaochen Chen <xiaochenchen.git@gmail.com>
2014-02-13 12:54:52 -0500

    solve extra problem for search in rotated array, adding unit test cases

--- src/main/java/xcc/leetcode/SearchInRotatedArray.java
+++ src/main/java/xcc/leetcode/SearchInRotatedArray.java
@@ -1,6 +1,7 @@
 package xcc.leetcode;
 
 public class SearchInRotatedArray {
+
 	public int search(int[] A, int target)
 	{
 		int l = 0;
@@ -29,5 +30,37 @@ public class SearchInRotatedArray {
 		
 		return -1;
 	}
+
+//    Challenge with an extended problem
+//
+//    Implement the following function, FindSortedArrayRotation,
+//    which takes as its input an array of unique integers that has been sorted in ascending order,
+//    then rotated by an unknown amount X where 0 <= X <= (arrayLength - 1).
+//    An array rotation by amount X moves every element array[i] to array[(i + X) % arrayLength].
+//    FindSortedArrayRotation discovers and returns X by examining the array.
+
+    public int findSortedArrayRotation(int[] A)
+    {
+        int l = 0, r = A.length - 1;
+
+        // if the array is sorted, will return immediately
+        while(A[l] > A[r])
+        {
+            int m = (l + r) / 2;
+
+            // check the right half first, if right half is not sorted, set l = m + 1
+            if(A[m] > A[r])
+            {
+                l = m + 1;
+            }
+            // else continue check left half
+            else
+            {
+                r = m;
+            }
+        }
+
+        return l;
+    }
 	
 }
